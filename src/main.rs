3f4ef1011fbba76d4e0c7dc312d1e8a0cddec6ed
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-08-05 00:55:09 +0400

    Print "command not found"

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,14 @@ fn main() -> Result<(), anyhow::Error> {
     print!("$ ");
     io::stdout().flush()?;
 
-    // Wait for user input
+    let registered_commands: Vec<String> = Vec::new();
+
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    stdin.read_line(&mut input)?;
+    if !registered_commands.contains(&input) {
+        println!("{}: command not found", input.trim());
+    }
     Ok(())
 }
 
