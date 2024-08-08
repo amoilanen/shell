6098fc0efd2db6f4f475fe9ae3ee2a642a133d73
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-08-09 00:47:01 +0400

    Add initial REPL loop

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use std::io::{self, Write};
 
 fn main() -> Result<(), anyhow::Error> {
-    print!("$ ");
-    io::stdout().flush()?;
-
     let registered_commands: Vec<String> = Vec::new();
 
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input)?;
-    if !registered_commands.contains(&input) {
-        println!("{}: command not found", input.trim());
+    loop {
+        let stdin = io::stdin();
+        let mut input = String::new();
+        print!("$ ");
+        io::stdout().flush()?;
+        stdin.read_line(&mut input)?;
+        if !registered_commands.contains(&input) {
+            println!("{}: command not found", input.trim());
+        }
     }
-    Ok(())
+    //Ok(())
 }
 
