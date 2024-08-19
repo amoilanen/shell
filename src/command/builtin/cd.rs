05a117b2a7cfb59fcb8af56cc3effdf7482b6a9c
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-08-20 00:43:02 +0400

    Implement cd to the home directory

--- src/command/builtin/cd.rs
+++ src/command/builtin/cd.rs
@@ -6,12 +6,11 @@ pub(crate) struct Cd {}
 
 impl ShellCommand for Cd {
     fn run(&self, command_and_args: &Vec<&str>) -> () {
-        //TODO: If len == 0, then navigate to the home directory
-        if command_and_args.len() > 0 {
+        let home_directory = path::Path::new(&env::var("HOME").unwrap()).to_path_buf();
+        let destination_directory = if command_and_args.len() > 1 {
             let destination = command_and_args[1].trim().to_string();
             let destination_parts = destination.split("/");
             let mut current_directory = env::current_dir().unwrap();
-    
             for (idx, destination_part) in destination_parts.into_iter().enumerate() {
                 if destination_part == "." {
                     //Do nothing, already in the correct directory (current directory)
@@ -19,15 +18,20 @@ impl ShellCommand for Cd {
                     current_directory.pop();
                 } else if destination_part == "" && idx == 0 {
                     current_directory = path::Path::new("/").to_path_buf();
+                } else if destination_part == "~" && idx == 0 {
+                    current_directory = home_directory.clone();
                 } else {
                     current_directory.push(destination_part);
                 }
-            };
-            match env::set_current_dir(current_directory.clone()) {
-                Ok(_) => (),
-                Err(_) =>
-                    println!("cd: {}: No such file or directory", current_directory.to_string_lossy())
             }
+            current_directory
+        } else {
+            home_directory
+        };
+        match env::set_current_dir(destination_directory.clone()) {
+            Ok(_) => (),
+            Err(_) =>
+                println!("cd: {}: No such file or directory", destination_directory.to_string_lossy())
         }
     }
     fn name(&self) -> String {
