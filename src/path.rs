09548cae33a8d3e83f4ec521fae33c525dfbcb9d
Anton Moilanen <anton.a.moilanen@gmail.com>
2025-08-10 20:04:35 +0400

    Check also that file is executable

--- src/path.rs
+++ src/path.rs
@@ -1,4 +1,7 @@
 use std::path;
+use std::fs;
+use std::os::unix::fs::PermissionsExt;
+use std::path::PathBuf;
 
 #[derive(Debug, Clone)]
 pub(crate) struct Path {
@@ -19,12 +22,24 @@ impl Path {
         while next_directory.is_some() && found_command.is_none() {
             if let Some(directory) = next_directory {
                 let path_to_command = path::Path::new(directory).join(command_name);
-                if path_to_command.is_file() {
+                if self.is_executable(&path_to_command).unwrap_or(false) {
                     found_command = path_to_command.to_str().map(|x| x.to_string());
                 }
             }
             next_directory = iterator.next();
         }
         found_command
+    }
+
+    fn is_executable(&self, path: &PathBuf) -> std::io::Result<bool> {
+        let metadata = fs::metadata(path)?;
+        let permissions = metadata.permissions();
+
+        if !metadata.is_file() {
+            return Ok(false);
+        }
+    
+        let mode = permissions.mode();
+        Ok(mode & 0o111 != 0)
     }
 }
